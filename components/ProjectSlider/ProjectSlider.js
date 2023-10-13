65ce0d39ea9ab99c16470f1ae3cd530883e87917
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-10-13 16:22:16 +0300

    Improvements on projectSlider

--- components/ProjectSlider/ProjectSlider.js
+++ components/ProjectSlider/ProjectSlider.js
@@ -2,42 +2,48 @@
 import styles from './ProjectSlider.module.css';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { animateTextChange } from '@/utils/animateTextChange';
 import { AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
 import arrowIcon from '../../public/arrow-icon.svg'
-import blur from '../../public/blur.png'
 
 export default function ProjectSlider({ projectArray, english }) {
   const [projectIndex, setProjectIndex] = useState(0);
 
-  const content = projectArray.map((project, index) => {
-    if (projectIndex === index) {
-      return (
-        <motion.div className={styles.mainpage_project_img_container} key={index} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.3 }}>
-          <div className={styles.project_mainpage_text_container}>
-            <h4 className={styles.project_mainpage_text_header}>{project.name}</h4>
-            {animateTextChange(styles.project_mainpage_text_description, english, project.description, project.fin_description)}
-            <p className={styles.project_mainpage_text_link}>
-                Learn More
-                <Image height={20} className={styles.project_mainpage_text_link_img} src={arrowIcon} alt="arrow icon" />
-            </p>
-          </div>
-          <Image 
-            fill 
-            priority 
-            placeholder='blur' 
-            src={project.displayImage[0]} 
-            blurDataURL={'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8NUGpHgAF5QIN93np7gAAAABJRU5ErkJggg=='}
-            className={styles.project_mainpage_image} 
-            alt="wide-project-img" 
-          />
-        </motion.div>
-      );
-    }
-    return null;
-  });
+  const content = () => {
+    const project = projectArray[projectIndex];
+  
+    console.log('Rendering content for project:', project.name);  // Debug line
+  
+    return (
+      <motion.div 
+        className={styles.mainpage_project_img_container}
+        key={projectIndex}  // Consider using a more unique key
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className={styles.project_mainpage_text_container}>
+          <h4 className={styles.project_mainpage_text_header}>{project.name}</h4>
+          {animateTextChange(styles.project_mainpage_text_description, english, project.description, project.fin_description)}
+          <p className={styles.project_mainpage_text_link}>
+            Learn More
+            <Image height={20} className={styles.project_mainpage_text_link_img} src={arrowIcon} alt="arrow icon" />
+          </p>
+        </div>
+        <Image 
+          fill 
+          priority 
+          placeholder='blur' 
+          src={project.displayImage[0]} 
+          blurDataURL={'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8NUGpHgAF5QIN93np7gAAAABJRU5ErkJggg=='}
+          className={styles.project_mainpage_image} 
+          alt="wide-project-img" 
+        />
+      </motion.div>
+    );
+  };
 
   const changeProject = (index) => {
     console.log(index);
@@ -80,9 +86,9 @@ export default function ProjectSlider({ projectArray, english }) {
         <div className={styles.mobile_header}>
           <h3 className={styles.header_inner}>{animateTextChange('', english, 'Real Estate Development', 'Kiinteistökehitys')}</h3>
         </div>
-        <Link href={projectArray[projectIndex].projectPage} className={styles.mainpage_project_link}>
-          {content}
-        </Link>
+        <div className={styles.mainpage_project_link}>
+          {content()}
+        </div>
         <select className={styles.mobile_project_select} value={projectIndex} onChange={(e) => changeProject(e.target.value)}>
           {projectArray.map((project, index) => {
             return (
