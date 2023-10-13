cda74c65ca71e3ba1d69b810488fc7da5b6da64b
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-10-13 14:48:04 +0300

    Neuterl textChange

--- utils/animateTextChange.js
+++ utils/animateTextChange.js
@@ -1,29 +1,24 @@
-import { AnimatePresence, motion } from 'framer-motion'
-
 export const animateTextChange = (className, english, eng, fin) => {
     if (english) {
         return (
-                <AnimatePresence mode='wait'>
-                    <motion.p
+                    <p
                     className={className}
-                    key="red"
                     initial={{opacity: 0}}
                     animate={{opacity: 1}}
-                    exit={{ opacity: 0 }}
-                    >{eng}</motion.p>
-                </AnimatePresence>
+                    exit={{ opacity: 0 }}>
+                        {eng}
+                    </p>
         )
 
     } else {
         return (
-            <AnimatePresence mode='wait'>
-                <motion.p
-                    className={className}
-                    key="blue"
-                    initial={{opacity: 0}}
-                    animate={{opacity: 1}}
-                    exit={{ opacity: 0 }}>{fin}</motion.p>
-            </AnimatePresence>
+                <p
+                className={className}
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                exit={{ opacity: 0 }}>
+                    {fin}
+                </p>
         )
     }
 
