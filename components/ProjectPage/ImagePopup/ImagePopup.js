037d4f96010478dc2901ad8be3e763ffa7817c36
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-10-17 12:09:52 +0300

    Button tweak on projectPage slider

--- components/ProjectPage/ImagePopup/ImagePopup.js
+++ components/ProjectPage/ImagePopup/ImagePopup.js
@@ -15,10 +15,10 @@ export default function ImagePopup({ popupOpen, setPopupOpen, arrayObject, index
       <Transition show={popupOpen} as={Fragment}>
         <Dialog open={popupOpen} onClose={() => setPopupOpen(false)} className={styles.dialog}>
           <Dialog.Overlay className={styles.dialog_overlay} />
-          <button className={`${styles.swiper_button_prev} ${styles.popup_button}`}>
+          <button className={`${styles.swiper_button_prev}`}>
             <MdKeyboardArrowLeft className={styles.popup_button_icon} />
           </button>
-          <button className={`${styles.swiper_button_next} ${styles.popup_button}`}>
+          <button className={`${styles.swiper_button_next}`}>
             <MdKeyboardArrowRight className={styles.popup_button_icon} />
           </button>
           <div id='image-modal' className={styles.modal} onClick={() => setPopupOpen(false)}>
