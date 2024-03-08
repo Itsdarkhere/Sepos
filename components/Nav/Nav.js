f0590a5ebf8b4d7f87a4355601d646c8a5dc8e0c
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-03-08 14:07:01 +0200

    Better sidenav

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,39 +1,62 @@
 'use client'
 import styles from './Nav.module.css'
-import NavPopup from './NavPopup/Navpopup'
-import { useState } from 'react'
-import { NavIcon } from './NavIcon/NavIcon'
-import { motion } from 'framer-motion'
 import Link from 'next/link'
+import { ProjectArray } from '@/utils/Array'
+import { useState } from 'react'
 
 export default function Nav({ lang }) {
-    const [popupOpen, setPopupOpen] = useState(false);
+    const projectArray = ProjectArray()
+    const [isOpen, setIsOpen] = useState(false)
 
-    /* //opening pop up both opens it and gives the index of the array that contains the info for the popup */
-    const activatePopup = () => {
-        if (popupOpen) {
-            setPopupOpen(false);
-        } else {
-            setPopupOpen(true);
-        }
+    const toggleDrawer = () => {
+        setIsOpen(!isOpen)
     }
 
-    const scrollToTop = () => {
-        window.scrollTo(0, 0);
-    }
-
-
-    /* //Chooses between burger and string menu based on width */
     const getNav = () => {
-        return (    
-            <motion.button className={styles.hamburger_holder} aria-label="Open Navigation"
-            initial={false} onClick={activatePopup} animate={popupOpen ? "open" : "closed"} >  
-                <NavIcon />
-                <NavPopup popupOpen={popupOpen} lang={lang} />
-            </motion.button>
-            )
-    
-    }
+    return (
+      <div className="drawer flex justify-end">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" onChange={toggleDrawer} />
+        <label htmlFor="my-drawer" className="btn btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className={`inline-block w-6 h-6 stroke-current transition-transform duration-300 ${isOpen ? 'transform rotate-90' : ''}`}>
+            {isOpen ? (
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+            ) : (
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            )}
+          </svg>
+        </label>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+          <ul className="menu px-4 py-8 w-80 min-h-full bg-zinc-900 text-base-content">
+            <li>
+              <Link href="/#projects" className="text-gray-200 font-semibold text-lg rounded-none border-b border-yellow-600 hover:bg-yellow-600 hover:text-zinc-800 transition duration-300">
+                {lang === 'en' ? 'Real Estate Developments' : 'Kiinteistökehitys'}
+              </Link>
+            </li>
+            <div className="pl-4 mt-2">
+              {projectArray.map((project, index) => (
+                <li key={index}>
+                  <Link href={project.projectPage} className="text-gray-300 rounded-none leading-8 hover:bg-yellow-600 hover:text-zinc-800 transition duration-300">
+                    {project.name}
+                  </Link>
+                </li>
+              ))}
+            </div>
+            <li className="mt-4">
+              <Link href="/#companies" className="text-gray-200 font-semibold text-lg rounded-none border-b border-yellow-600 hover:bg-yellow-600 hover:text-zinc-800 transition duration-300">
+                {lang === 'en' ? 'Affiliated Companies' : 'Osakkuusyhtiöt'}
+              </Link>
+            </li>
+            <li className="mt-4">
+              <Link href="/#contact" className="text-gray-200 font-semibold text-lg rounded-none border-b border-yellow-600 hover:bg-yellow-600 hover:text-zinc-800 transition duration-300">
+                {lang === 'en' ? 'Contact' : 'Yhteystiedot'}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
 
     return (
         <div className={styles.navbar_container} id="navbar">
