9bc9322b3e33f67e5c0cd264f6afaf1780e49c29
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-06-04 11:05:17 +0300

    contact card improvements

--- components/Contacts/Person/Person.js
+++ components/Contacts/Person/Person.js
@@ -1,17 +1,33 @@
-import styles from './Person.module.css'
-import { motion } from 'framer-motion';
+import styles from "./Person.module.css";
+import Image from "next/image";
+import klemmari from "../../../public/klemm.svg";
+import contact from "../../../public/cont.svg";
+import mail from "../../../public/email.svg";
+import { motion } from "framer-motion";
 
 export default function Person({ name, phone, email }) {
-    return (
-        <motion.div className={styles.person_container}>
-            <div className={styles.d}>
-                <p className={styles.person_name}>{name}</p>
-            </div>
-            <div className={styles.p}>
-                { phone ?
-                <a href={'tel:' +  phone} className={styles.number_contact}>{phone}</a> : null }
-                <a href={'mailto:' + email} className={styles.email_contact}>{email}</a>
-            </div>
-        </motion.div>
-    );
+  return (
+    <motion.div className=' rounded-md border border-neutral-900 px-4 py-6 shadow-lg'>
+      <div className='flex flex-row justify-between mb-4 text-2xl'>
+        <p className=''>{name}</p>
+        <Image src={klemmari} alt='logo' />
+      </div>
+      <div className=' flex flex-col gap-2 text-xs text-neutral-400'>
+        {phone && (
+          <div className='flex flex-row gap-2'>
+            <Image src={contact} alt='contact' />
+            <a href={"tel:" + phone} className=''>
+              {phone}
+            </a>
+          </div>
+        )}
+        <div className='flex flex-row gap-2'>
+          <Image src={mail} alt='mail' />
+          <a href={"mailto:" + email} className=''>
+            {email}
+          </a>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
