dc00194c94e1b6dc3e3fc7aa0dfb00d3785a47fa
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-06-05 14:05:21 +0300

    Better projects

---
+++ components/ProjectSlider/Project.js
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import arrowIcon from "../../public/arrow-icon.svg";
+import styles from "./ProjectSlider.module.css";
+
+export default function Project({project, lang, projectIndex}) {
+  return (
+    <motion.div
+      className={
+        "w-full h-full relative flex justify-start items-end overflow-hidden cursor-pointer"
+      }
+      key={projectIndex} // Consider using a more unique key
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
+    >
+      <div
+        className={
+          " w-full z-10 px-4 md:px-8 py-6 md:py-24 flex flex-col justify-center items-start md:items-center"
+        }
+      >
+        <h4 className={" font-semibold text-3xl mb-2"}>{project.name}</h4>
+        <p
+          className={
+            " max-w-[600px] line-clamp-4 mb-8 text-sm md:text-base font-light text-neutral-200 text-start md:text-center"
+          }
+        >
+          {lang === "en"
+            ? project.english.description
+            : project.finnish.description}
+        </p>
+        <Link
+          href={project.projectPage}
+          className={
+            " flex flex-row text-sm border bg-opacity-40 bg-neutral-800 px-4 py-2 border-orange-400 rounded-md gap-2 items-center"
+          }
+        >
+          {lang === "en" ? "Learn More" : "Lue lisää"}
+          <Image height={16} className={""} src={arrowIcon} alt='arrow icon' />
+        </Link>
+      </div>
+      <Image
+        fill
+        priority
+        placeholder='blur'
+        src={project.displayImage[0]}
+        blurDataURL={
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8NUGpHgAF5QIN93np7gAAAABJRU5ErkJggg=="
+        }
+        className={styles.project_mainpage_image}
+        alt='wide-project-img'
+      />
+    </motion.div>
+  );
+}
--- components/ProjectSlider/ProjectSlider.js
+++ components/ProjectSlider/ProjectSlider.js
@@ -1,45 +1,14 @@
-'use client'
-import styles from './ProjectSlider.module.css';
-import { motion } from 'framer-motion';
-import React, { useState } from 'react';
-import Image from 'next/image';
-import arrowIcon from '../../public/arrow-icon.svg'
-import { Listbox } from '@headlessui/react';
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import Link from "next/link";
+"use client";
+import React, { useState } from "react";
+import Project from "./Project";
 
 export default function ProjectSlider({ projectArray, lang }) {
   const [projectIndex, setProjectIndex] = useState(0);
 
   const content = () => {
     const project = projectArray[projectIndex];
-  
     return (
-      <motion.div 
-        className={styles.mainpage_project_img_container}
-        key={projectIndex}  // Consider using a more unique key
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.3 }}
-      >
-        <div className={styles.project_mainpage_text_container}>
-          <h4 className={styles.project_mainpage_text_header}>{project.name}</h4>
-          <p className={styles.project_mainpage_text_description}>{lang === 'en' ? project.english.description : project.finnish.description}</p>
-          <Link href={project.projectPage} className={styles.project_mainpage_text_link}>
-            {lang === 'en' ? 'Learn More' : 'Lue lisää'}
-            <Image height={20} className={styles.project_mainpage_text_link_img} src={arrowIcon} alt="arrow icon" />
-          </Link>
-        </div>
-        <Image 
-          fill 
-          priority 
-          placeholder='blur' 
-          src={project.displayImage[0]} 
-          blurDataURL={'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8NUGpHgAF5QIN93np7gAAAABJRU5ErkJggg=='}
-          className={styles.project_mainpage_image} 
-          alt="wide-project-img" 
-        />
-      </motion.div>
+      <Project project={project} lang={lang} projectIndex={projectIndex} />
     );
   };
 
@@ -48,64 +17,51 @@ export default function ProjectSlider({ projectArray, lang }) {
   };
 
   const checkButtonSelected = (index) => {
-    return index === projectIndex ? `${styles.project_button} ${styles.selected}` : styles.project_button;
+    return index === projectIndex
+      ? `${" text-neutral-200 text-sm border-b border-b-orange-400 px-4 py-2"}`
+      : "text-neutral-400 text-sm border-b border-b-neutral-800 px-4 py-2";
   };
 
   return (
-    <div className={styles.project_slider_outer_container} id="projects">
-      <div className={styles.project_slider_inner_container}>
-        <div className={styles.project_slider_container}>
-          <div className={styles.header}>
-            <h3 className={" text-base md:text-3xl font-semibold text-neutral-300"}>
-              {lang === 'en' ? (
-                <p>
-                    Real Estate <br /> Development
-                </p>
-              ) : (
-                <p>
-                    Kiinteistökehitys
-                </p>
-              )}
-            </h3>
+    <div
+      className={
+        " w-full bg-neutral-800 flex justify-center items-center py-16 px-6"
+      }
+      id='projects'
+    >
+      <div className={" w-full max-w-7xl flex flex-col"}>
+        <div className={" text-start md:text-center"}>
+          <h3
+            className={
+              " text-base md:text-3xl mb-4 md:mb-6 font-semibold text-neutral-300"
+            }
+          >
+            {lang === "en" ? "Real Estate Development" : "Kiinteistökehitys"}
+          </h3>
+          <div className=' hidden md:flex flex-row w-full justify-center gap-4 mb-8'>
+            {projectArray.map((project, index) => {
+              return (
+                <button
+                  className={checkButtonSelected(index)}
+                  key={index}
+                  onClick={() => changeProject(index)}
+                >
+                  {project.name}
+                </button>
+              );
+            })}
           </div>
-          {projectArray.map((project, index) => {
-            return (
-              <button className={checkButtonSelected(index)} key={index} onClick={() => changeProject(index)}>
-                {project.name}
-              </button>
-            );
-          })}
         </div>
-        <div className={styles.mobile_header}>
-          <h3 className={styles.header_inner}>{lang === 'en' ? 'Real Estate Development' : 'Kiinteistökehitys'}</h3>
+        {/* DESKTOP */}
+        <div className='hidden md:block w-full h-full'>{content()}</div>
+        {/* MOBILE */}
+        <div className="w-full h-full flex flex-col gap-4 md:hidden">
+            {projectArray.map((project, i) => {
+              return (
+                <Project key={i} project={project} lang={lang} projectIndex={i} />
+              )
+            })}
         </div>
-        <div className={styles.mainpage_project_link}>
-          {content()}
-        </div>
-        <Listbox as="div" className={styles.listbox} value={projectIndex} onChange={setProjectIndex}>
-            {({ open }) => (
-            <>
-                <Listbox.Button className={styles.listbox_button}> 
-                    {projectArray[projectIndex].name}
-                    <ChevronUpDownIcon className={styles.listboxButtonIcon} />
-                </Listbox.Button>
-                <Listbox.Options className={styles.listbox_options}>
-                {projectArray.map((project, index) => (
-                    <Listbox.Option key={index} value={index} className={styles.listbox_option}>
-                    {({ selected }) => (
-                        <>
-                        {project.name}
-                        {selected ? (
-                            <CheckIcon className={styles.selectedIcon} aria-hidden="true" />
-                          ) : null}
-                        </>
-                    )}
-                    </Listbox.Option>
-                ))}
-                </Listbox.Options>
-            </>
-            )}
-        </Listbox>
       </div>
     </div>
   );
