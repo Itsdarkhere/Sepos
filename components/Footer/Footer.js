8f6e75d9a616ac419dd245c4e04d8a13164efb73
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-06-05 12:57:54 +0300

    clean footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,18 +1,61 @@
-import styles from './Footer.module.css'
+import Link from "next/link";
+import styles from "./Footer.module.css";
 
 export default function Footer({ english }) {
+  const RE_ARRAY = [
+    { name: "Garden Helsinki", link: "" },
+    { name: "Hangon Läntinen Laituri", link: "" },
+    { name: "Hippos Jyväskylä", link: "" },
+    { name: "Turun Ratapiha  ", link: "" },
+    { name: "Hämeensaari", link: "" },
+    { name: "Inkoonranta", link: "" },
+  ];
 
-    return (
-        <footer className={styles.bottom_outer_container}>
-            <div className={styles.bottom_inner_container}>
-                <div className={styles.footer_logo_button}
-                aria-label="Home Button">
-                    <p className="align">SEPOS OY</p>
-                </div>
-                <div className={styles.footer_tags_container}>
-                    <div className={styles.footer_befound}>BeFound Web Design</div>
-                </div>
+  const COMPANY_ARRAY = [
+    { name: "Modum Oy", link: "" },
+    { name: "LUV Kodit Oy", link: "" },
+    { name: "NPC", link: "" },
+    { name: "Hög", link: "" },
+  ];
+  return (
+    <footer
+      className={" py-12 px-6 flex justify-center items-center bg-neutral-900"}
+    >
+      <div className=" flex flex-col-reverse lg:flex-row gap-8 justify-between max-w-7xl w-full">
+        <div className='flex flex-col gap-8 justify-start'>
+          <div className='flex flex-col gap-4'>
+            <h5>Real Estate Projects</h5>
+            <div className=' flex flex-row flex-wrap gap-4'>
+              {RE_ARRAY.map((project, i) => {
+                return (
+                  <FooterLink key={i} name={project.name} link={project.link} />
+                );
+              })}
             </div>
-        </footer>
-    )
+          </div>
+          <div className='flex flex-col gap-4'>
+            <h5>Affiliated Companies</h5>
+            <div className=' flex flex-row flex-wrap gap-4'>
+              {COMPANY_ARRAY.map((company, i) => {
+                return (
+                  <FooterLink key={i} name={company.name} link={company.link} />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className={styles.footer_logo_button} aria-label='Home Button'>
+          <p className='align'>SEPOS OY</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+function FooterLink({ name, link }) {
+  return (
+    <Link href={link} className=' text-neutral-500 text-xs font-light'>
+      {name}
+    </Link>
+  );
 }
