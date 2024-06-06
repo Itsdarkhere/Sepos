5dd62c62186c4cbf94ec8b1f34ce567efe049bd7
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-06-06 12:51:41 +0300

    image modal tweaks

--- components/ProjectPage/ImagePopup/ImagePopup.js
+++ components/ProjectPage/ImagePopup/ImagePopup.js
@@ -1,27 +1,46 @@
-'use client'
-import styles from './ImagePopup.module.css';
-import React, { Fragment } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import { Navigation } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from 'react-icons/md';
-import Image from 'next/image';
-import 'swiper/css/navigation';
-import 'swiper/css';
+"use client";
+import styles from "./ImagePopup.module.css";
+import React, { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
+import Image from "next/image";
+import "swiper/css/navigation";
+import "swiper/css";
 
-export default function ImagePopup({ popupOpen, setPopupOpen, arrayObject, index }) {
+export default function ImagePopup({
+  popupOpen,
+  setPopupOpen,
+  arrayObject,
+  index,
+}) {
   return (
     <div id='modal_container'>
       <Transition show={popupOpen} as={Fragment}>
-        <Dialog open={popupOpen} onClose={() => setPopupOpen(false)} className={styles.dialog}>
+        <Dialog
+          open={popupOpen}
+          onClose={() => setPopupOpen(false)}
+          className={styles.dialog}
+        >
           <Dialog.Overlay className={styles.dialog_overlay} />
-          <button className={`${styles.swiper_button_prev} h-auto btn btn-ghost`} id="swiper-button-prev">
-            <MdKeyboardArrowLeft className={styles.popup_button_icon} />
+          <button
+            className={`${styles.swiper_button_prev} hidden md:flex h-auto btn btn-ghost rounded-md`}
+            id='swiper-button-prev'
+          >
+            <MdKeyboardArrowLeft className={" h-10 w-10"} />
           </button>
-          <button className={`${styles.swiper_button_next} h-auto btn btn-ghost`} id="swiper-button-next">
-            <MdKeyboardArrowRight className={styles.popup_button_icon} />
+          <button
+            className={`${styles.swiper_button_next} hidden md:flex h-auto btn btn-ghost rounded-md`}
+            id='swiper-button-next'
+          >
+            <MdKeyboardArrowRight className={" h-10 w-10"} />
           </button>
-          <div id='image-modal' className={styles.modal} onClick={() => setPopupOpen(false)}>
+          <div
+            id='image-modal'
+            className={"w-11/12 h-4/5 md:w-4/5 flex justify-center items-center pointer-events-none"}
+            onClick={() => setPopupOpen(false)}
+          >
             <div className={styles.image_loader}></div>
             <Swiper
               modules={[Navigation]}
@@ -41,10 +60,15 @@ export default function ImagePopup({ popupOpen, setPopupOpen, arrayObject, index
             >
               {arrayObject.pictures2.map((src, index) => (
                 <SwiperSlide
-                  className={`${styles.modal_slide} swiper-slide swiper-lazy`}
+                  className={` flex justify-center items-center swiper-slide swiper-lazy`}
                   key={index}
                 >
-                  <Image fill className={`${styles.popup_gallery_image} swiper-lazy`} src={src} alt='project' />
+                  <Image
+                    fill
+                    className={` object-contain rounded-3xl cursor-pointer pointer-events-auto swiper-lazy `}
+                    src={src}
+                    alt='Real estate project'
+                  />
                 </SwiperSlide>
               ))}
             </Swiper>
@@ -54,8 +78,3 @@ export default function ImagePopup({ popupOpen, setPopupOpen, arrayObject, index
     </div>
   );
 }
-
-
-
-
-
