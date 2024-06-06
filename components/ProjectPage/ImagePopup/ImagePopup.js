4abfbe8b27a42d1d399e90f6671b223a63cb0053
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-06-06 13:53:11 +0300

    no nav

--- components/ProjectPage/ImagePopup/ImagePopup.js
+++ components/ProjectPage/ImagePopup/ImagePopup.js
@@ -2,9 +2,7 @@
 import styles from "./ImagePopup.module.css";
 import React, { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
 import Image from "next/image";
 import "swiper/css/navigation";
 import "swiper/css";
@@ -24,41 +22,27 @@ export default function ImagePopup({
           className={styles.dialog}
         >
           <Dialog.Overlay className={styles.dialog_overlay} />
-          <button
-            className={`${styles.swiper_button_prev} hidden md:flex h-auto btn btn-ghost rounded-md`}
-            id='swiper-button-prev'
-          >
-            <MdKeyboardArrowLeft className={" h-10 w-10"} />
-          </button>
-          <button
-            className={`${styles.swiper_button_next} hidden md:flex h-auto btn btn-ghost rounded-md`}
-            id='swiper-button-next'
-          >
-            <MdKeyboardArrowRight className={" h-10 w-10"} />
-          </button>
           <div
             id='image-modal'
-            className={"w-11/12 h-4/5 md:w-4/5 flex justify-center items-center pointer-events-none"}
-            onClick={() => setPopupOpen(false)}
+            className={
+              "w-11/12 h-4/5 md:w-4/5 flex justify-center items-center pointer-events-none"
+            }
+            // onClick={() => setPopupOpen(false)}
           >
             <div className={styles.image_loader}></div>
             <Swiper
-              modules={[Navigation]}
               spaceBetween={50}
               direction='horizontal'
               slidesPerView={1}
               slidesPerGroup={1}
-              speed={300}
+              speed={200}
               pagination={false}
               loop={true}
               observer={true}
               observeParents={true}
               parallax={true}
               initialSlide={index}
-              navigation={{
-                nextEl: `#swiper-button-next`,
-                prevEl: `#swiper-button-prev`,
-              }}
+              navigation={false}
               className={styles.modal_swiper}
             >
               {arrayObject.pictures2.map((src, index) => (
@@ -68,7 +52,7 @@ export default function ImagePopup({
                 >
                   <Image
                     fill
-                    className={` object-contain rounded-3xl cursor-pointer pointer-events-auto swiper-lazy `}
+                    className={` object-contain cursor-pointer pointer-events-auto swiper-lazy `}
                     src={src}
                     alt='Real estate project'
                   />
