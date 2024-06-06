d3cf98ad47b407a82a5f547384d5533b17808138
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-06-06 12:11:28 +0300

    characters for contact

--- components/Contacts/Contacts.js
+++ components/Contacts/Contacts.js
@@ -1,25 +1,29 @@
 'use client'
-import styles from './Contacts.module.css';
 import Person from './Person/Person';
 import React from 'react';
+import dude1 from '../../public/dude1.svg'
+import dude2 from '../../public/dude2.svg'
+import dude3 from '../../public/dude3.svg'
+import dude4 from '../../public/dude4.svg'
+import dude5 from '../../public/dude5.svg'
+
 
 export default function Contacts({ lang }) {
     
     return (
         <div className={"w-full bg-neutral-800 py-16 px-6 flex justify-center items-center"}  id="contact">
             <div className='w-full max-w-7xl'>
-                <div className={""}>
+                <div className={" mb-4 md:mb-8"}>
                     <div className={" text-base md:text-3xl font-semibold text-neutral-300"}>
                         <p>{lang === 'en' ? 'Contact Us' : 'Yhteystiedot'}</p>
                     </div>
                 </div>
-                <div className={styles.persons_grid}>
-                    <Person image="Bean.jpeg" name="Jouni Alho" phone={'+358 50 3054104'} email="jouni.alho@sepos.fi" />
-                    <Person image="Bean.jpeg" name="Juha Pajunen"  phone={'+358 40 7441272'} email="juha.pajunen@axecon.fi" />    
-                    <Person image="Bean.jpeg" name="Ilkka Kilpimaa"  phone={'+358 40 7435223'} email="ilkka.kilpimaa@sepos.fi" />
-                    <Person image="Bean.jpeg" name="Timo Everi"  phone={'+358 40 8600231'} email="timo.everi@sepos.fi" />
-                    <Person image="Bean.jpeg" name="Jorma Alanne" email="jorma.alanne@taaleri.com" />
-                    <div className={styles.under_550_spacer}></div>
+                <div className={"w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-6 gap-4"}>
+                    <Person cl={" col-span-1 xl:col-span-3"} dude={dude1} image="Bean.jpeg" firstname={"Jouni"} lastname={"Alho"}  phone={'+358 50 3054104'} email="jouni.alho@sepos.fi" />
+                    <Person cl={" col-span-1 xl:col-span-3"} dude={dude2} image="Bean.jpeg" firstname={"Juha"} lastname={"Pajunen"}  phone={'+358 40 7441272'} email="juha.pajunen@axecon.fi" />    
+                    <Person cl={" col-span-1 xl:col-span-2"} dude={dude3} image="Bean.jpeg" firstname={"Ilkka"} lastname={"Kilpimaa"}  phone={'+358 40 7435223'} email="ilkka.kilpimaa@sepos.fi" />
+                    <Person cl={" col-span-1 xl:col-span-2"} dude={dude4} image="Bean.jpeg" firstname={"Timo"} lastname={"Everi"}  phone={'+358 40 8600231'} email="timo.everi@sepos.fi" />
+                    <Person cl={" col-span-1 xl:col-span-2"} dude={dude5} image="Bean.jpeg" firstname={"Jorma"} lastname={"Alanne"} email="jorma.alanne@taaleri.com" />
                 </div>
             </div>
         </div>
--- components/Contacts/Person/Person.js
+++ components/Contacts/Person/Person.js
@@ -1,16 +1,29 @@
-import styles from './Person.module.css'
 import { motion } from 'framer-motion';
+import mail from '../../../public/mail.svg'
+import phoneIcon from '../../../public/phone.svg'
+import Image from 'next/image';
 
-export default function Person({ name, phone, email }) {
+export default function Person({ cl, firstname, lastname, phone, email, dude }) {
     return (
-        <motion.div className={"flex w-full flex-col gap-4 bg-neutral-900 rounded-md py-6 px-4"}>
-            <div className={" text-base font-bold"}>
-                <p className={" text-white"}>{name}</p>
+        <motion.div className={`${cl} flex w-full flex-row gap-4 justify-between bg-neutral-900 rounded-md py-6 px-6`}>
+            <div className=' flex flex-col h-full items-start justify-start gap-4 '>
+                <div className={" text-lg font-bold"}>
+                    <p className={" text-white"}>{firstname} <span className=' text-orange-400'>{lastname}</span></p>
+                </div>
+                <div className={"flex flex-col justify-start gap-1 text-xs text-neutral-400 font-normal"}>
+                    { phone && (
+                        <div className='flex flex-row gap-2'>
+                            <Image height={14} src={phoneIcon} alt='phone' />
+                            <a href={'tel:' +  phone} className={""}>{phone}</a>
+                        </div>
+                    )}
+                    <div className='flex flex-row gap-2'>
+                            <Image height={14} src={mail} alt='email' />
+                            <a href={'mailto:' + email} className={""}>{email}</a>
+                        </div>
+                </div>
             </div>
-            <div className={"flex flex-col justify-start gap-1 text-xs text-neutral-400 font-normal"}>
-                { phone && <a href={'tel:' +  phone} className={""}>{phone}</a>}
-                <a href={'mailto:' + email} className={""}>{email}</a>
-            </div>
+            <Image height={100} src={dude} alt='' />
         </motion.div>
     );
 }
