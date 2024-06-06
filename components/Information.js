332bfbcc922a8fc49248e2f16ea7f960904e5c28
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-06-06 15:15:51 +0300

    try this

--- components/Information.js
+++ components/Information.js
@@ -1,6 +1,6 @@
 export default function Information() {
   return (
-    <div className="block md:hidden w-full bg-neutral-800 px-6 py-12">
+    <div className="block md:hidden w-full bg-neutral-900 px-6 py-12">
       <h4 className=" font-light text-neutral-400">
         <span className=" font-medium text-orange-400">Sepos Oy</span> is a company founded in 2014 and owned by Timo Everi, Ilkka
         Kilpimaa and NasdaqOMX Helsinki listed Taaleri Sijoitus Oy. The
